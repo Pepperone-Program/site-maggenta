9b01c0a9906432a9fef16ad4895958d585821a61
Dérick Campos Santos <derickcampossantos1@gmail.com>
2026-05-29 11:19:56 -0300

    adiciona gtm em todas as paginas

---
+++ src/lib/google-tags.ts
@@ -0,0 +1,5 @@
+export const googleTagManagerId =
+  process.env.NEXT_PUBLIC_GTM_ID || "GTM-T24NRLNQ";
+
+export const ga4Id = process.env.NEXT_PUBLIC_GA4_ID;
+export const googleAdsId = process.env.NEXT_PUBLIC_GOOGLE_ADS_ID;
