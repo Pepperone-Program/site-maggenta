1a185ff363f008785b90f7748c6f89481f2c75a4
Dérick Campos Santos <derickcampossantos1@gmail.com>
2026-05-29 11:34:02 -0300

    adiciona gtag

--- src/lib/google-tags.ts
+++ src/lib/google-tags.ts
@@ -1,5 +1,6 @@
 export const googleTagManagerId =
   process.env.NEXT_PUBLIC_GTM_ID || "GTM-T24NRLNQ";
 
-export const ga4Id = process.env.NEXT_PUBLIC_GA4_ID;
+export const googleAnalyticsId =
+  process.env.NEXT_PUBLIC_GA4_ID || "G-KPTW37WV4S";
 export const googleAdsId = process.env.NEXT_PUBLIC_GOOGLE_ADS_ID;
